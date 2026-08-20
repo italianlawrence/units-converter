4e294fa487c1e6429894b619be0be30793aaa6c7
italianlawrence <314434779+italianlawrence@users.noreply.github.com>
2026-08-20 09:48:36 +0200

    Modified toFixed() inner value

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const poundsInKilos = document.getElementById('pounds-in-kilos')
 convertBtn.addEventListener('click', function() {
     const inputValue = Number(document.getElementById('input-nmb').value)
     
-    metersInFeet.textContent = inputValue + ' meters = ' + (inputValue * 3.281).toFixed(2) + ' feets'
-    feetInMeters.textContent = inputValue + ' feets = ' + (inputValue / 3.281).toFixed(2) + ' meters'
+    metersInFeet.textContent = inputValue + ' meters = ' + (inputValue * 3.281).toFixed(3) + ' feets'
+    feetInMeters.textContent = inputValue + ' feets = ' + (inputValue / 3.281).toFixed(3) + ' meters'
 
-    litersInGallons.textContent = inputValue + ' liters = ' + (inputValue / 3.785).toFixed(2) + ' gallons'
-    gallonsInLiters.textContent = inputValue + ' gallons = ' + (inputValue * 3.785).toFixed(2) + ' liters'
+    litersInGallons.textContent = inputValue + ' liters = ' + (inputValue / 3.785).toFixed(3) + ' gallons'
+    gallonsInLiters.textContent = inputValue + ' gallons = ' + (inputValue * 3.785).toFixed(3) + ' liters'
 
-    kilosInPounds.textContent = inputValue + ' kilos = ' + (inputValue * 2.205).toFixed(2) + ' pounds'
-    poundsInKilos.textContent = inputValue + ' pounds = ' + (inputValue / 2.205).toFixed(2) + ' kilos'
-    
+    kilosInPounds.textContent = inputValue + ' kilos = ' + (inputValue * 2.205).toFixed(3) + ' pounds'
+    poundsInKilos.textContent = inputValue + ' pounds = ' + (inputValue / 2.205).toFixed(3) + ' kilos'
+
 })
